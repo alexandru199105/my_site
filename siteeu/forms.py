b2f3e571137b04596ab251d86ca10f3b5aa24274
alex <purcareaalex91@yahoo.com>
2024-06-10 20:11:37 +0300

    changed my form

--- siteeu/forms.py
+++ siteeu/forms.py
@@ -1,8 +1,11 @@
 from django import forms
 from django.core.validators import EmailValidator
 
+
 class ContactForm(forms.Form):
     nume = forms.CharField(max_length=50, required=True)
     email = forms.EmailField(required=True)
     mesaj = forms.CharField(widget=forms.Textarea, required=True)
-    nr_telefon = forms.CharField(max_length=15 ,required=True)
+    nr_telefon = forms.CharField(max_length=15 ,required=True)  
+    
+    
