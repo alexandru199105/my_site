11fa09c47dcc1c49567e82720f180ad4d32361c6
alex <purcareaalex91@yahoo.com>
2024-06-10 20:10:14 +0300

    second commit

--- siteeu/wsgi.py
+++ siteeu/wsgi.py
@@ -11,6 +11,8 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'siteeu.settings')
+settings_module ='siteeu.deployment' if 'WEBSITE_HOSTNAME' in os.environ else 'siteeu.settings'
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', settings_module)
 
 application = get_wsgi_application()
