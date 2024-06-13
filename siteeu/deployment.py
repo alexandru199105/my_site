f9ab6708efe9f01236151b3a51b2f7ea3cd5d1e6
alex <purcareaalex91@yahoo.com>
2024-06-13 14:34:51 +0300

    deployment file modified to see error logs

--- siteeu/deployment.py
+++ siteeu/deployment.py
@@ -1,9 +1,11 @@
 import os
+import logging
+
 from .settings import *
 from .settings import BASE_DIR
 
-ALLOWED_HOSTS = [os.environ['WEBSITE_HOSTNAME']]
-CSRF_TRUSTED_ORIGINS = ['https://' + os.environ['WEBSITE_HOSTNAME']]
+ALLOWED_HOSTS = [os.environ.get('WEBSITE_HOSTNAME', '')]
+CSRF_TRUSTED_ORIGINS = ['https://' + os.environ.get('WEBSITE_HOSTNAME', '')]
 DEBUG = False
 
 MIDDLEWARE = [
@@ -20,17 +22,21 @@ MIDDLEWARE = [
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
-# Parse the connection string into a dictionary
-connection_string = os.environ['AZURE_POSTGRESQL_CONNECTIONSTRING']
-parameters = {pair.split('=')[0]: pair.split('=')[1] for pair in connection_string.split(' ')}
+logger = logging.getLogger('django')
 
-# Define the database settings using the parsed parameters
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': parameters['dbname'],
-        'HOST': parameters['host'],
-        'USER': parameters['user'],
-        'PASSWORD': parameters['password'],
+try:
+    connection_string = os.environ['AZURE_POSTGRESQL_CONNECTIONSTRING']
+    parameters = {pair.split('=')[0]: pair.split('=')[1] for pair in connection_string.split(' ')}
+    
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.postgresql',
+            'NAME': parameters['dbname'],
+            'HOST': parameters['host'],
+            'USER': parameters['user'],
+            'PASSWORD': parameters['password'],
+        }
     }
-}
+except Exception as e:
+    logger.error("Error setting up database configuration: %s", str(e))
+    raise e
